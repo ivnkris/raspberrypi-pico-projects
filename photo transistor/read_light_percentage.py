65754060c8e77071aefd60c06424a071d12ccae4
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-01-01 14:38:24 +0000

    added read_light_percentage.py for photo transistor projects

---
+++ photo transistor/read_light_percentage.py
@@ -0,0 +1,14 @@
+#Imports
+from machine import ADC, Pin
+from time import sleep
+
+# Define ADC Pin for light sensor
+lightsensor = ADC(Pin(26))
+
+# Read analouge sensor value
+light = lightsensor.read_u16()
+
+# Turn reading into percentage
+lightpercent = light / 65535 * 100
+
+print(lightpercent)
