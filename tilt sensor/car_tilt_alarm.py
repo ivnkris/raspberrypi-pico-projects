0be3a3431659fc18a3f534ad7ca29de81ac8809a
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-02-26 20:57:49 +0000

    added car tilt alarm

---
+++ tilt sensor/car_tilt_alarm.py
@@ -0,0 +1,17 @@
+from machine import Pin, PWM
+import time
+
+# Set up tilt sensor Pin
+tilt = Pin(26, Pin.IN, Pin.PULL_DOWN)
+
+# Set up buzzer
+buzzer = PWM(Pin(13))
+buzzer.freq(1000)
+
+while True:
+    time.sleep(0.01)
+    if tilt.value() == 1:
+        print("***TILT DETECTED!***")
+        buzzer.duty_u16(10000)
+        time.sleep(0.2)
+        buzzer.duty_u16(0)
